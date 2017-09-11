f8da2763cabef37a50d71a6b8773d765f802f6f2
annacanal <31701548+annacanal@users.noreply.github.com>
2017-09-11 14:13:39 +0200

    hmm2 update
    - noves funcions a Matrix.java
    - Hmm2 bàsic finiquitat

--- hmm/HMM2.java
+++ hmm/HMM2.java
@@ -1,14 +1,142 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hmm;
 
-/**
- *
- * @author USUARIO
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
 public class HMM2 {
-    
+
+	  
+    public static void solve_hmm2() {
+            Matrix A, B, pi, O, delta, delta_idx;
+            String line;
+            String [] buf;
+            int rows, cols;
+            BufferedReader reader;
+            
+            //readInput(A,B,pi);
+            try{
+                //System.out.println("Reading input...");
+            	reader = new BufferedReader(new InputStreamReader(System.in));
+                //reader = new BufferedReader(new FileReader("src/hmm/input2.txt"));
+
+                // read and fill matrix A
+                line = reader.readLine();
+                buf = line.split(" ");
+                rows = Integer.parseInt(buf[0]);
+                cols = Integer.parseInt(buf[1]);
+                A = new Matrix(rows, cols);
+                A.fillMatrix(buf, 2);
+              //  A.show();
+
+                // read and fill matrix B
+                line = reader.readLine();
+                buf = line.split(" ");
+                rows = Integer.parseInt(buf[0]);
+                cols = Integer.parseInt(buf[1]);
+                B = new Matrix(rows, cols);
+                B.fillMatrix(buf, 2);
+               // B.show();
+
+                // read and fill matrix pi
+                line = reader.readLine();
+                buf = line.split(" ");
+                rows = Integer.parseInt(buf[0]);
+                cols = Integer.parseInt(buf[1]);
+                pi = new Matrix(rows, cols);
+                pi.fillMatrix(buf, 2);
+              //  pi.show();
+                
+                // read and fill matrix O (observations)
+                line = reader.readLine();
+                buf = line.split(" ");
+                rows = Integer.parseInt(buf[0]);
+                cols = 1;
+                O = new Matrix(rows, cols);
+                O.fillMatrix(buf, 1);
+              //  O.show();
+                
+                
+                // Initialize alpha matrix
+                delta = new Matrix(A.getRows(), O.getRows());
+                delta_idx = new Matrix(A.getRows(), O.getRows());
+                delta.fillColumn(pi.mult_column(B, O.getIntElement(0,0)),0);   
+              //  delta.fillColumn(pi.mult_column_log(B, O.getIntElement(0,0)),0);    
+                // delta_idx first column is -1 (we don't care)
+                delta_idx.fillColumnSameNumber(-1,delta_idx.getRows(),0);
+                //delta_idx.show();
+                Matrix aux = new Matrix(A.getCols(),1);
+                double temp_max = 0.0;
+               // double temp_max = -100;
+                double current = 0.0;
+                int max_state = -1;
+                
+                
+                // Iterate over all observations
+                for (int i = 1; i < O.getRows(); i++){
+                	/* System.out.print("Valor i: ");
+                	 System.out.println(i);*/
+                    for (int j = 0; j < A.getCols(); j++){
+                    	/* System.out.print("Valor j: ");
+                    	 System.out.println(j);*/
+                    	 temp_max = 0.0;
+                    	//temp_max = -100;
+                        for (int k = 0; k < A.getRows(); k++){
+                        /*	System.out.print("Valor k: ");
+                        	 System.out.println(k);*/
+                        	 current = A.getElement(k, j)*delta.getElement(k, i-1);
+                        	//current = Math.log(A.getElement(j, k)) + delta.getElement(k, i-1);
+                          //  System.out.println(current);
+                            if(temp_max<current){
+                                temp_max = current;
+                                max_state = k;
+                            }
+                         /*   System.out.print("Valor max_current: ");
+                            System.out.println(temp_max);*/
+                        }
+                        if (temp_max * B.getElement(j, O.getIntElement(i, 0)) == 0){  	
+                        	max_state = -1; //I don't care, 
+                        }/*
+                        if (temp_max + Math.log( B.getElement(j, O.getIntElement(i, 0))) == 0){  	
+                        	max_state = -1; //I don't care, 
+                        }*/
+                        delta_idx.setValue(j, i, max_state);
+                        aux.setValue(j, 0, temp_max);
+                        //multiply by B-column according to observation
+                        delta.fillColumn(aux.mult_column(B, O.getIntElement(i, 0)),i);
+                      //  delta.fillColumn(aux.plus_column_log(B, O.getIntElement(j, O.getIntElement(i,0))),i);
+                        
+                    }
+                    
+                }
+             /*   System.out.println("delta: ");
+                delta.show();
+                System.out.println("delta index: "); 
+                delta_idx.show();   */   
+                
+                // Decide most probable state sequence
+                Matrix sequence = new Matrix(1,O.getRows());
+                double max = 0.0;     
+                //last position
+                for (int i = 0; i < delta.getRows(); i++){
+                    if(delta.getElement(i, delta.getCols()-1)>max){
+                        max = delta.getElement(i, delta.getCols()-1);
+                        sequence.setValue(0,sequence.getCols()-1,i);
+                    }
+                }
+              //  sequence.show();
+                
+                for (int i = O.getRows()-1; i > 0; i--){
+                    sequence.setValue(0,i-1,delta_idx.getElement(sequence.getIntElement(0, i), i));
+                }
+                
+                // Show result of HMM2
+                sequence.showHMM2solution();
+                
+            }catch(Exception e){
+                // Something went wrong
+            }    
+    }
+
+
 }
--- hmm/Matrix.java
+++ hmm/Matrix.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hmm;
 
-/**
- *
- * @author USUARIO
- */
 final public class Matrix {
     private final int M;             // number of rows
     private final int N;             // number of columns
@@ -34,7 +25,25 @@ final public class Matrix {
         return (int) this.data[row][col];
     }
     
+    public double getElement (int row, int col){
+        return data[row][col];
+    }
     
+    public Matrix getColumn (int col){
+        Matrix A = new Matrix(this.M, 1);
+        for (int i=0; i < M; i++){
+            A.setValue(i, 0, data[i][col]);
+        }
+        return A;
+    }
+    
+    
+    // SETTERS
+    public void setValue(int x, int y, double value){
+        data[x][y] = value;
+    }
+    
+
     // METHODS
     // fill matrix with values from String []
     public void fillMatrix (String [] buf, int start_data){
@@ -46,12 +55,18 @@ final public class Matrix {
         }
     }
     
-    // swap rows i and j
-    private void swap(int i, int j) {
-        double[] temp = data[i];
-        data[i] = data[j];
-        data[j] = temp;
-    }    
+    // fill column with values from a column matrix
+    public void fillColumn (Matrix B, int col){
+        for (int i=0; i < M; i++){
+            data[i][col] = B.getElement(i, 0);
+        }
+    }
+    // fill column with values from a column matrix
+    public void fillColumnSameNumber (double num, int num_rows, int col){
+        for (int i=0; i < num_rows; i++){
+            data[i][col] = num;
+        }
+    }
     
     // create and return the transpose of the invoking matrix
     public Matrix transpose() {
@@ -70,6 +85,16 @@ final public class Matrix {
         for (int i = 0; i < M; i++)
             for (int j = 0; j < N; j++)
                 C.data[i][j] = A.data[i][j] + B.data[i][j];
+        return C;
+    }
+
+    // return C = log(A) + log(B) = log (A·B)
+    public Matrix plus_column_log(Matrix B,  int col) {
+        Matrix A = this;
+        if (B.M != A.M || B.N != A.N) throw new RuntimeException("Illegal matrix dimensions.");
+        Matrix C = new Matrix(M, N);
+        for (int i = 0; i < M; i++)
+        	C.data[i][col] = Math.log(A.data[i][col]) + Math.log(B.data[i][col]);
         return C;
     }
 
@@ -106,11 +131,23 @@ final public class Matrix {
         return C;
     }
     
+ // return C = log(A * B)
+    public Matrix multiply_log(Matrix B) {
+        Matrix A = this;
+        if (A.N != B.M) throw new RuntimeException("Illegal matrix dimensions.");
+        Matrix C = new Matrix(A.M, B.N);
+        for (int i = 0; i < C.M; i++)
+            for (int j = 0; j < C.N; j++)
+                for (int k = 0; k < A.N; k++)
+                    C.data[i][j] += Math.log((A.data[i][k] * B.data[k][j]));
+        return C;
+    }
+    
     // return C = A * B[:][col] (multiplies by a column of B, elementwise)
     public Matrix mult_column(Matrix B, int col){
         Matrix A = this;
         if(A.N > 1 && A.M > 1){
-            System.out.println("First matrix has to be a row or a column.");
+           // System.out.println("First matrix has to be a row or a column.");
         }else if(A.N>1){ //if it is a row
             A = A.transpose();
         }
@@ -121,6 +158,36 @@ final public class Matrix {
         return C;
     }
     
+    // return C = log( A * B[:][col]) (multiplies by a column of B, elementwise)
+    public Matrix mult_column_log(Matrix B, int col){
+        Matrix A = this;
+        if(A.N > 1 && A.M > 1){
+           // System.out.println("First matrix has to be a row or a column.");
+        }else if(A.N>1){ //if it is a row
+            A = A.transpose();
+        }
+        Matrix C = new Matrix (B.M, 1);
+        for (int i = 0; i< B.M; i++){
+            C.data[i][0] = Math.log (A.data[i][0] * B.data[i][col]);
+        }
+        return C;
+    }
+    
+    // return max of the element-wise product
+    public Matrix max(){
+        Matrix max = new Matrix(M,1);
+        for (int i=0; i < M; i++){
+            for (int j=0; j< N; j++){
+                    if(data[i][j]>max.getElement(i,0)){
+                        max.setValue(i, 0, data[i][j]);
+                    }
+            }
+        }
+        return max;
+    }
+    
+    
+    // PRINTING METHODS TO SHOW SOLUTIONS:
     // print matrix to standard output
     public void show() {
         for (int i = 0; i < M; i++) {
@@ -138,7 +205,7 @@ final public class Matrix {
             for (int j = 0; j < N; j++) 
                 solution += data[i][j] + " ";
         }
-        System.out.println(solution);
+        System.out.print(solution);
     }
     
     //print matrix to HMM1 output: sums up all elements and prints
@@ -149,5 +216,33 @@ final public class Matrix {
                 solution += data[i][j];
         }
         System.out.println(solution);
+    }
+    
+    //print matrix to HMM2 output 
+    public void showHMM2solution(){
+        String solution = "";
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) 
+                solution += this.getIntElement(i, j) + " ";
+        }
+        System.out.print(solution);
+    }
+    
+    //print matrix A and B to HMM3 output
+    public void showHMM3solution(Matrix B){
+        String solution = "";
+        System.out.print(M+" "+ N+" ");
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) 
+                solution += this.getIntElement(i, j) + " ";
+        }
+        System.out.print(solution);
+        solution = "";
+        System.out.print(B.getRows()+" "+ B.getCols()+" ");
+        for (int i = 0; i < B.getRows(); i++) {
+            for (int j = 0; j <  B.getCols(); j++) 
+                solution += B.getIntElement(i, j) + " ";
+        }
+        System.out.print(solution);
     }
 }
